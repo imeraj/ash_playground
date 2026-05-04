213916e16394af6f9c5e07d5a7936b1bd46e8b0f
Meraj Molla <meraj.enigma@gmail.com>
2026-05-04 06:25:04 -0400

    aggregates

--- tunez/lib/tunez_web/live/artists/index_live.ex
+++ tunez/lib/tunez_web/live/artists/index_live.ex
@@ -17,7 +17,11 @@ defmodule TunezWeb.Artists.IndexLive do
     page_params = AshPhoenix.LiveView.params_to_page_opts(params, default_limit: 12, count?: true)
 
     page =
-      Tunez.Music.search_artists!(query_text, page: page_params, query: [sort_input: sort_by])
+      Tunez.Music.search_artists!(query_text,
+        page: page_params,
+        query: [sort_input: sort_by],
+        load: [:album_count, :latest_album_year_released, :cover_image_url]
+      )
 
     socket =
       socket
@@ -90,7 +94,7 @@ defmodule TunezWeb.Artists.IndexLive do
     ~H"""
     <div id={"artist-#{@artist.id}"} data-role="artist-card" class="relative mb-2">
       <.link navigate={~p"/artists/#{@artist.id}"}>
-        <.cover_image />
+        <.cover_image image={@artist.cover_image_url} />
       </.link>
     </div>
     <p class="flex justify-between">
@@ -102,6 +106,7 @@ defmodule TunezWeb.Artists.IndexLive do
         {@artist.name}
       </.link>
     </p>
+    <.artist_card_album_info artist={@artist} />
     """
   end
 
@@ -200,7 +205,9 @@ defmodule TunezWeb.Artists.IndexLive do
     [
       {"recently updated", "-updated_at"},
       {"recently added", "-inserted_at"},
-      {"name", "name"}
+      {"name", "name"},
+      {"number of albums", "-album_count"},
+      {"latest album release", "--latest_album_year_released"}
     ]
   end
 
