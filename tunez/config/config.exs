289a159b80195da0c0d03c37a9b6abf708b5387e
Meraj Molla <meraj.enigma@gmail.com>
2026-05-10 17:58:49 -0400

    authentication apis

--- tunez/config/config.exs
+++ tunez/config/config.exs
@@ -36,6 +36,9 @@ config :spark,
     remove_parens?: true,
     "Ash.Resource": [
       section_order: [
+        :authentication,
+        :token,
+        :user_identity,
         :graphql,
         :json_api,
         :postgres,
@@ -71,7 +74,7 @@ config :spark,
 config :tunez,
   ecto_repos: [Tunez.Repo],
   generators: [timestamp_type: :utc_datetime],
-  ash_domains: [Tunez.Music]
+  ash_domains: [Tunez.Accounts, Tunez.Music]
 
 # Configures the endpoint
 config :tunez, TunezWeb.Endpoint,
