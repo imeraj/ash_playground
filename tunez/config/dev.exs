6f0c7cc588890baadbfb12687c0f6cff89241e2b
Meraj Molla <meraj.enigma@gmail.com>
2026-05-10 18:50:22 -0400

    magic link auth

--- tunez/config/dev.exs
+++ tunez/config/dev.exs
@@ -1,6 +1,8 @@
 import Config
 config :ash, policies: [show_policy_breakdowns?: true]
 
+config :ash_authentication, debug_authentication_failures?: true
+
 # Configure your database
 config :tunez, Tunez.Repo,
   username: "meraj",
